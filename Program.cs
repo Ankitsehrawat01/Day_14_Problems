c4976beccd8416f7d07e4d36db5db623fd91d3cc
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-19 16:20:44 +0530

    Queue using linkedlist

--- Program.cs
+++ Program.cs
@@ -7,24 +7,30 @@ namespace Day_14_Practice_Problem
     {
         public static void Main(string[] args)
         {
-            Stack_Ex stackobj = new Stack_Ex();
-            stackobj.Push(70);
-            stackobj.Push(30);
-            stackobj.Push(56);
-            stackobj.Display();
-            stackobj.Peek();
-            stackobj.Pop();
-            stackobj.Display();
-            stackobj.Peek();
-            stackobj.Pop();
-            stackobj.Display();
-            stackobj.IsEmpty();
+            Queue_Ex queueobj = new Queue_Ex();
+            queueobj.Enqueue(56);
+            queueobj.Enqueue(30);
+            queueobj.Enqueue(70);
+            queueobj.Display();
 
-            //Linkedlist a1 = new Linkedlist();
-            //a1.AddNode(56);
-            //a1.AddNode(30);
-            //a1.AddNode(70);
-            //a1.Display();
+            //Stack_Ex stackobj = new Stack_Ex();
+            //stackobj.Push(70);
+            //stackobj.Push(30);
+            //stackobj.Push(56);
+            //stackobj.Display();
+            //stackobj.Peek();
+            //stackobj.Pop();
+            //stackobj.Display();
+            //stackobj.Peek();
+            //stackobj.Pop();
+            //stackobj.Display();
+            //stackobj.IsEmpty();
+
+            //Linkedlist linkedlistobj = new Linkedlist();
+            //linkedlistobj.AddNode(56);
+            //linkedlistobj.AddNode(30);
+            //linkedlistobj.AddNode(70);
+            //linkedlistobj.Display();
 
             //LinkedList<int> a1 = new LinkedList<int>();
             //a1.AddLast(56);
---
+++ Queue_Ex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_Practice_Problem
+{
+    public class Queue_Ex
+    {
+        Node rear;
+        Node front;
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (front == null)
+            {
+                rear = newnode;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = newnode;
+                rear = rear.Next;
+            }
+        }
+        public void Display()
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            Node temp = front;
+            while (temp != null)
+            {
+                Console.Write("| " + temp.data + " |");
+                temp = temp.Next;
+            }
+        }
+    }
+}
