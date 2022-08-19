6bea7357d2f6464704194fd6182c12d4b987b439
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-19 15:12:59 +0530

    Using_Nodes

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_Practice_Problem
+{
+    public class Node
+    {
+        public int data;
+        public Node Next;
+        public Node(int data)
+        {
+            this.data = data;
+            Next = null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,15 +7,21 @@ namespace Day_14_Practice_Problem
     {
         public static void Main(string[] args)
         {
-            LinkedList<int> a1 = new LinkedList<int>(); 
-            a1.AddLast(56);
-            a1.AddLast(30);
-            a1.AddLast(70);
-            foreach (int data in a1)
-            {
-                Console.WriteLine( "| " + data + " |");
-                Console.WriteLine("-----------------");
-            }
+            Linkedlist a1 = new Linkedlist();
+            a1.AddNode(56);
+            a1.AddNode(30);
+            a1.AddNode(70);
+            a1.Display();
+
+            //LinkedList<int> a1 = new LinkedList<int>();
+            //a1.AddLast(56);
+            //a1.AddLast(30);
+            //a1.AddLast(70);
+            //foreach (int data in a1)
+            //{
+            //    Console.WriteLine( "| " + data + " |");
+            //    Console.WriteLine("-----------------");
+            //}
         }
     }
 }
