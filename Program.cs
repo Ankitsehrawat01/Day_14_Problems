b667434228db141df5b3f539bdb79481d8038e06
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-19 14:14:02 +0530

    Linkedlist of 56, 30, 70

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_14_Practice_Problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> a1 = new LinkedList<int>(); 
+            a1.AddLast(56);
+            a1.AddLast(30);
+            a1.AddLast(70);
+            foreach (int data in a1)
+            {
+                Console.WriteLine( "| " + data + " |");
+                Console.WriteLine("-----------------");
+            }
+        }
+    }
+}
